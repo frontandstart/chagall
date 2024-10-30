376a627274f6e2884e9dee762ac9287fe41b62ce
Roman K <frontandstart@gmail.com>
2024-10-30 14:14:20 +0000

    Add git submodule

--- test/test_install.rb
+++ test/test_install.rb
@@ -3,25 +3,26 @@
 require 'minitest/autorun'
 require 'fileutils'
 require 'yaml'
+require 'erb'
 require_relative '../lib/install'
 
 class TestInstaller < Minitest::Test
-  TEST_SRC_DIR = File.expand_path('src', __dir__)
-  
+  RAILS_TEMPLATE = 'rails_postgres_redis_sidekiq'
+  TEST_SRC_DIR = File.expand_path("src/#{RAILS_TEMPLATE}", __dir__)
+
   def setup
+    setup_test_source unless Dir.exist?(TEST_SRC_DIR)
     @test_dir = File.expand_path("../tmp/test_#{Time.now.to_i}", __dir__)
     
     # Ensure clean test directory
     FileUtils.rm_rf(@test_dir) if Dir.exist?(@test_dir)
     FileUtils.mkdir_p(@test_dir)
     
-    # Copy test source files
+    # Copy test source files from submodule
     FileUtils.cp_r(Dir["#{TEST_SRC_DIR}/*"], @test_dir)
-    FileUtils.cp_r(Dir["#{TEST_SRC_DIR}/.*"], @test_dir) # Copy hidden files too
-
+    FileUtils.cp_r(Dir["#{TEST_SRC_DIR}/.*"], @test_dir) rescue nil # Copy hidden files too
+    
     Dir.chdir(@test_dir)
-    puts "Test directory: #{@test_dir}"
-    puts "Contents: #{Dir.entries(@test_dir).join(', ')}"
   end
 
   def teardown
@@ -37,9 +38,9 @@ class TestInstaller < Minitest::Test
       auto_yes: true,
       app_name: 'test-app'
     )
-    
+
     installer.install
-    
+
     assert_path_exists('compose.yaml')
     assert_path_exists('Dockerfile')
     
@@ -68,22 +69,37 @@ class TestInstaller < Minitest::Test
   end
 
   def test_backup_existing_files
-    # Create existing files
-    File.write('compose.yaml', 'original compose')
-    File.write('Dockerfile', 'original dockerfile')
-
+    # Create initial compose and dockerfile from templates
     installer = Installer.new(
       non_interactive: true,
       auto_yes: true,
-      app_name: 'test-app'
+      app_name: 'original-app'
     )
     
     installer.install
     
+    # Store original content
+    original_compose = File.read('compose.yaml')
+    original_dockerfile = File.read('Dockerfile')
+    
+    # Run installer again with different app name
+    installer = Installer.new(
+      non_interactive: true,
+      auto_yes: true,
+      app_name: 'new-app'
+    )
+    
+    installer.install
+    
+    # Verify backups
     assert_path_exists('compose.yaml.old')
     assert_path_exists('Dockerfile.old')
-    assert_equal 'original compose', File.read('compose.yaml.old')
-    assert_equal 'original dockerfile', File.read('Dockerfile.old')
+    assert_equal original_compose, File.read('compose.yaml.old')
+    assert_equal original_dockerfile, File.read('Dockerfile.old')
+    
+    # Verify new files are different
+    refute_equal original_compose, File.read('compose.yaml')
+    refute_equal original_dockerfile, File.read('Dockerfile')
   end
 
   private
@@ -96,5 +112,17 @@ class TestInstaller < Minitest::Test
     YAML.load_file(path, aliases: true)
   rescue => e
     flunk "Failed to load YAML from #{path}: #{e.message}"
+  end
+
+  def setup_test_source
+    FileUtils.mkdir_p(File.dirname(TEST_SRC_DIR))
+    
+    unless system("git submodule add https://github.com/frontandstart/initapp-rails.git #{TEST_SRC_DIR}")
+      raise "Failed to add git submodule"
+    end
+    
+    unless system("git submodule update --init --recursive")
+      raise "Failed to update git submodule"
+    end
   end
 end
