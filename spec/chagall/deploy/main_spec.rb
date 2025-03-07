72d8437747af44668d91cc758b45db24df97d776
Roman K <frontandstart@gmail.com>
2025-03-07 13:05:22 +0300

    start make spec

--- spec/chagall/deploy/main_spec.rb
+++ spec/chagall/deploy/main_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require 'spec_helper'
-require_relative '../../../chagall/lib/chagall/deploy/main'
+# require_relative '../../../chagall/lib/chagall/deploy/main'
 require 'stringio'
 
 RSpec.describe Chagall::Deploy::Main do
@@ -12,10 +12,10 @@ RSpec.describe Chagall::Deploy::Main do
   let(:tag) { SecureRandom.hex(4) }
 
   before do
-    allow(Settings).to receive(:[]).and_call_original
-    allow(Settings).to receive(:[]).with(:projects_folder).and_return("/tmp/projects/#{run_id}")
-    allow(Settings).to receive(:[]).with(:tag).and_return('v1')
-    allow(Settings).to receive(:[]).with(:server).and_return('localserver')
+    allow(Chagall::Settings).to receive(:[]).and_call_original
+    allow(Chagall::Settings).to receive(:[]).with(:projects_folder).and_return("/tmp/projects/#{run_id}")
+    allow(Chagall::Settings).to receive(:[]).with(:tag).and_return('v1')
+    allow(Chagall::Settings).to receive(:[]).with(:server).and_return('localserver')
     allow(main_instance).to receive(:run)
   end
 
@@ -48,7 +48,7 @@ RSpec.describe Chagall::Deploy::Main do
     context 'when dry_run is true and not forced' do
       before do
         # Simulate Settings[:dry_run] being true
-        allow(Chagall::Deploy::Settings).to receive(:[]).with(:dry_run).and_return(true)
+        allow(Chagall::Settings).to receive(:[]).with(:dry_run).and_return(true)
         # Stub system and ssh_execute to verify they are not called
         allow(main_instance).to receive(:system)
         allow(main_instance).to receive(:ssh_execute)
@@ -75,7 +75,7 @@ RSpec.describe Chagall::Deploy::Main do
 
     context 'when force is true even in dry_run mode' do
       before do
-        allow(Chagall::Deploy::Settings).to receive(:[]).with(:dry_run).and_return(true)
+        allow(Chagall::Settings).to receive(:[]).with(:dry_run).and_return(true)
         allow(main_instance).to receive(:system).and_return(true)
         allow(main_instance).to receive(:ssh_execute).and_return(true)
       end
--- spec/chagall/settings_spec.rb
+++ spec/chagall/settings_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require 'rspec'
-require_relative '../../../chagall/lib/chagall/deploy/settings'
+require_relative '../../../chagall/lib/chagall/settings'
 
 RSpec.describe Chagall::Deploy::Settings do
   before do
